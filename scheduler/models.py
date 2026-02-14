b33b98d8988cc4eff8203c2f9bf29ffe36468c70
Nathan Schoenike <natschoenike711@gmail.com>
2026-02-14 02:54:42 +0000

    Appointment Class model setup

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Appointment(models.model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    #//service can be Nails, Hair
+    service = models.CharField(max_length=30, default = "Acne Treatment")
+    date = models.DateField()
+    starttime = models.TimeField()
+    endtime = models.TimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.client.username} - {self.service} on {self.date} from {self.starttime} to {self.endtime}"    
