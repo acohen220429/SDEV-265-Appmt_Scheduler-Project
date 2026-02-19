f72a6478b6b0461b41e1617b115a1af3a5d7d76a
Nathan Schoenike <natschoenike711@gmail.com>
2026-02-19 00:06:15 +0000

    Added starting and closing times

--- scheduler/views.py
+++ scheduler/views.py
@@ -141,7 +141,7 @@ def create_appointment(request):
     else:
         form = AppointmentForm()
 
-    return render(request, "scheduler/create_appointment.html", {"form": form})
+return render(request, "scheduler/create_appointment.html", {"form": form})
 
 
 
